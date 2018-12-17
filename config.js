fc7c91ba19e6f9de45602e5565c3588eb2b0ade6
Ola <you@example.com>
2018-12-17 11:25:50 +0100

    Update event types definitions

---
+++ config.js
@@ -0,0 +1,20 @@
+
+const config = {
+  common: './events/common.json',
+  eventTypes: {
+    'domestic-transfer': './events/domestic-transfer.json',
+    'international-transfer': './events/international-transfer.json',
+    'tax-transfer': './events/tax-transfer.json',
+    'own-transfer': './events/own-transfer.json',
+    'pre-login': './events/pre-login',
+    'login': './events/login.json',
+    'logout': './events/logout.json',
+    'authorization': './events/authorization.json',
+    'send-sms-otp': './events/send-sms-otp.json',
+    'activation-internet': './events/activation-internet.json',
+    'card-activation': './events/card-activation.json',
+    'card-unblocking': './events/card-unblocking.json',
+  },
+};
+
+module.exports = config;
